c2cb04e4a6ef71ec1b5da1b401c6e9094857c2ef
Anirudh sagar <Anirudh2sagar@gmaikl.com>
2024-01-29 21:50:56 +0530

    upadtwe

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require("express")
 const mongoose =require("mongoose")
 const Users = require("./src/Models/User")
 const { register, login, findUser } = require("./src/Controlers/auths")
+const cors = require("cors");
 const server=express()
-const cors =require("cors")
+
 const { verifyToken } = require("./src/Middlewares")
 server.use(express.json())
-server.use(cors())
 
+
+
+
+server.use(cors());
 
 
 server.post("/register",register)
 server.post("/login",login)
 server.get("/get-user",verifyToken,findUser)
-server.listen("6000",()=>{
+server.listen("4000",()=>{
   console.log("server started")
 })
 mongoose
